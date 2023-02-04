52106be667429d866320363ab5be5e5c160916c0
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-02-04 17:20:24 +0330

    Add first person package

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+	RaycastHit hit;
+	public Camera fpsCamera;
+
+	void Awake ()
+	{
+	}
+
+	void Start ()
+	{
+	}
+		
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+		
+	GameObject getGameObject ()
+	{
+		bool raycastHit = Physics.Raycast (fpsCamera.transform.position, fpsCamera.transform.forward, out hit, 100);
+		if (raycastHit) {
+			return hit.collider.gameObject;
+		} else {
+			return null;
+		}
+	}
+
+	// shoot function isn't belong here, move it to weapon scripts later
+	public void Shoot ()
+	{
+		GameObject g = getGameObject ();
+		if (g == null)
+			return;
+		
+		Rigidbody rb = g.GetComponent<Rigidbody> ();
+		if (rb != null) {
+			Debug.Log ("Time Object : " + hit.collider.name);	
+			rb.AddForce (fpsCamera.transform.forward * 500);	
+		}
+	}
+
+	void ShowMessage (string msg)
+	{
+		Debug.Log (msg);
+	}
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Player))]
+public class PlayerController : MonoBehaviour
+{
+
+	[SerializeField] Player player;
+
+
+	void Awake ()
+	{
+		player = GetComponent<Player> ();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Control ();
+	}
+
+	void Control ()
+	{
+	}
+}
