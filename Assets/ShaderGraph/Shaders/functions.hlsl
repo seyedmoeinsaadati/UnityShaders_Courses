b4cc8516c101b31a2b2a3c579bc33344768871b9
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-02-18 15:21:42 +0330

    Add Sampel_Lighting

--- Assets/ShaderGraph/Shaders/functions.hlsl
+++ Assets/ShaderGraph/Shaders/functions.hlsl
@@ -1,3 +1,4 @@
+// testing
 void Add_float(in float a, in float b, out float _out)
 {
 	_out = a + b;
@@ -6,4 +7,17 @@ void Add_float(in float a, in float b, out float _out)
 void Mul_float(in float a, in float b, out float _out)
 {
 	_out = a * b;
+}
+
+// CustomLight.hlsl
+void CustomLight_float(out half3 direction)
+{
+#ifdef SHADERGRAPH_PREVIEW
+	direction = half3(0, 0, 0);
+#else
+#if defined(UNIVERSAL_LIGHTING_INCLUDED)
+	Light mainLight = GetMainLight();
+	direction = mainLight.direction;
+#endif
+#endif
 }
