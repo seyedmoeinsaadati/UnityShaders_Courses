725361f1506cba86d112e1402a0f58fdff51287c
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-05-26 22:48:09 +0330

    Add SkyboxLerpController.cs Modify some files

---
+++ Assets/MyShader/Skybox/SkyboxLerpController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxLerpController : MonoBehaviour
+{
+    [SerializeField] private float duration;
+    [SerializeField] private AnimationCurve t;
+
+    [SerializeField] private float min, max;
+    [SerializeField] private string lerpMaterialField = "_Lerp";
+
+    private Material skyboxMaterial;
+    private float timer, tValue;
+
+    private void Start()
+    {
+        skyboxMaterial = RenderSettings.skybox;
+    }
+
+    public void StartLerp()
+    {
+        enabled = true;
+    }
+
+    public void StopLerp()
+    {
+        timer = 0;
+        enabled = false;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime / duration;
+        tValue = Mathf.Lerp(min, max, t.Evaluate(timer));
+        skyboxMaterial.SetFloat(lerpMaterialField, tValue);
+        if (timer >= 1) StopLerp();
+    }
+
+}
