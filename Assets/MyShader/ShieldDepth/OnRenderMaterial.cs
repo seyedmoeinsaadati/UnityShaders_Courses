97438bcf4f262586854a5003e33f5c05d0ca763c
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-11-05 22:49:56 +0330

    Add Shield Shader sectoin Add ShowDepth

---
+++ Assets/MyShader/ShieldDepth/OnRenderMaterial.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class OnRenderMaterial : MonoBehaviour
+{
+    public Material EffectMaterial;
+
+    private void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, EffectMaterial);
+    }
+
+}
